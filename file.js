020245d5c9ef04915735282bb89879e0c172ec76
SANJANA2312 <sanjana@edu.sait.ca>
2024-11-15 17:18:53 -0700

    greetig message, and color added

--- file.js
+++ file.js
@@ -1,2 +1,22 @@
-const d = new Date();
-document.getElementById("date").innerHTML = d;
+// document.getElementById("unique_identifier").innerHTML = content
+const footer = document.getElementById ("footer");
+footer.innerHTML = `&copy; ${new Date ().getFullYear ()} Sanjana`;
+
+
+const date=new Date ()
+console.log ("Today's date",date );
+
+const hour=date.getFullYear()
+const greeting = document.getElementById ("portfolio-title")
+if (hour < 12) {
+    greeting.innerText = "Good Morning!, Welcome to my portfolio."
+    greeting.className = "morning"
+}
+else if (hour < 17) {
+    greeting.innerText = "Good Afternoon!, Welcome to my portfolio."
+    greeting.className = "afternoon"
+} 
+else {
+     greeting.innerText = "Good Evening!, Welcome to my portfolio."
+    greeting.className = "evening"
+}
