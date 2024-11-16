3aa69062d5724002fe5c812f79879f8d25770ca1
SANJANA2312 <sanjana@edu.sait.ca>
2024-11-15 17:54:45 -0700

    ordered list added

--- file.js
+++ file.js
@@ -34,3 +34,16 @@ btn.onmouseout = function() {
 btn.onclick = function() {
     alert("Always trust the Almighty! and stay hydrated.")
 }
+
+const orderedlist = document.getElementById ("numbers")
+for (let i = 1, i < = 12 , i++) { //more efficient syntax for incrementing the counter variable in loop
+    const listitem = document.createElement("li")
+    if (i% 2 == 0) {
+        listitem.innerText = "even"
+    } 
+     //double equal to added to check if remainder is qual to zero or not, if remainder 0, number is even
+     else {
+        listitem.innerText = "odd"
+     }
+     orderedlist.appendChild (listitem)
+}
